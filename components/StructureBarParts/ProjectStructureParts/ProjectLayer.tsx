4b10513231851998cf63a824089045eae0a98121
nekikara <nekikara@gmail.com>
2021-07-25 10:59:10 +0200

    To be pointer cursor

--- components/StructureBarParts/ProjectStructureParts/ProjectLayer.tsx
+++ components/StructureBarParts/ProjectStructureParts/ProjectLayer.tsx
@@ -31,7 +31,8 @@ export const ProjectLayer: React.VFC<Props> = ({ layer }) => {
           color: #bbb;
         }
         .projectLayerFrame:hover {
-          background-color: rgba(255, 255, 255, 0.5);
+          background-color: rgba(255, 255, 255, 0.1);
+          cursor: pointer;
         }
         .projectLayerInfo {
           padding: 5px 0;
