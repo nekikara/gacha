9d7c384cb41de04af0067738b56aad1078b82df4
nekikara <nekikara@gmail.com>
2021-08-08 13:14:30 +0200

    Uninstall dnd package

--- cypress/integration/platform_editor_spec.js
+++ cypress/integration/platform_editor_spec.js
@@ -1,3 +1,5 @@
+const dataTransfer = new DataTransfer()
+
 describe('Platform Editor Test', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
@@ -5,9 +7,12 @@ describe('Platform Editor Test', () => {
   })
   it('changes editor types', function () {
     cy.get('.toolItem').should('have.length', 1)
-    cy.get('#x1').drag('.board', { position: 'center' })
-    //   .trigger('pointerdown', { which: 1, button: 0 })
-    //   .trigger('dragstart')
-    // cy.get('.board').trigger('drop', 5, 10)
+    cy.get('#x1')
+      .trigger('pointerdown', { which: 1, button: 0 })
+      .trigger('dragstart', { dataTransfer, eventConstructor: 'DragEvent' })
+    cy.get('.board').trigger('drop', 200, 100, {
+      dataTransfer,
+      eventConstructor: 'DragEvent',
+    })
   })
 })
