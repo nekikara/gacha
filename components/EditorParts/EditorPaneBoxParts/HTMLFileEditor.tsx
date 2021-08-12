6bdb5ba3bbfe845522ef1d48f15b11c160633869
nekikara <nekikara@gmail.com>
2021-08-12 22:09:46 +0200

    Zoom

--- components/EditorParts/EditorPaneBoxParts/HTMLFileEditor.tsx
+++ components/EditorParts/EditorPaneBoxParts/HTMLFileEditor.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useRef } from 'react'
-import { Position } from '~/interfaces/position'
-
+import React from 'react'
+import { useTransformation } from './useTransformation'
 interface Size {
   w: number
   h: number
@@ -9,74 +8,13 @@ interface Size {
 interface Props {}
 
 export const HTMLFileEditor: React.VFC<Props> = ({}) => {
-  const canvasInnerRef = useRef<HTMLDivElement | null>(null)
-  const screenBaseRef = useRef<HTMLDivElement | null>(null)
-  const screenRef = useRef<HTMLDivElement | null>(null)
-
-  useEffect(() => {
-    console.log('======')
-    let screenWidth = 360
-    let scale = 1.0
-    const containerSize = { w: 0, h: 0 }
-    const canvasInnerScrollPosition: Position = { x: 100000, y: 100000 }
-
-    const canvasInner = canvasInnerRef.current
-    const screenBase = screenBaseRef.current
-    const screen = screenRef.current
-
-    function transformScreenBase() {
-      if (screenBase) {
-        const translateSize = {
-          w: containerSize.w - screenWidth,
-          h: containerSize.h,
-        }
-        const translateStr = `translate(${translateSize.w / 2}px, ${
-          translateSize.h / 2
-        }px)`
-        screenBase.style.transform = `${translateStr} scale(${scale})`
-      }
-    }
-
-    if (canvasInner) {
-      canvasInner.scrollTo(
-        canvasInnerScrollPosition.x,
-        canvasInnerScrollPosition.y
-      )
-      const { width, height } = canvasInner.getBoundingClientRect()
-      containerSize.w = width
-      containerSize.h = height
-      canvasInner.addEventListener(
-        'wheel',
-        function (ev: WheelEvent) {
-          ev.preventDefault()
-          if (ev.ctrlKey) {
-            console.log('==== point', { x: ev.clientX, y: ev.clientY })
-            const newScale = scale - ev.deltaY * 0.01
-            scale = newScale > 0.05 ? newScale : 0.05
-            transformScreenBase()
-          } else {
-            canvasInnerScrollPosition.x += ev.deltaX
-            canvasInnerScrollPosition.y += ev.deltaY
-            canvasInner.scrollTo(
-              canvasInnerScrollPosition.x,
-              canvasInnerScrollPosition.y
-            )
-          }
-        },
-        { passive: false }
-      )
-    }
-
-    if (screen) {
-      screen.style.width = `${screenWidth}px`
-      transformScreenBase()
-    }
-  }, [canvasInnerRef, screenRef, screenBaseRef])
+  const { boardRef, canvasInnerRef, screenBaseRef, screenRef } =
+    useTransformation()
 
   return (
     <>
-      <div ref={canvasInnerRef} className="editorHTMLFileBoard">
-        <div className="canvasInner">
+      <div ref={boardRef} className="editorHTMLFileBoard">
+        <div ref={canvasInnerRef} className="canvasInner">
           <div ref={screenBaseRef} className="screenBase">
             <div ref={screenRef} className="screen">
               <main className="setupFrame">HTMLFileEditor</main>
